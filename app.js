be7d3375c5caf9d62807f3820bc2c7e6fb1a8c37
aso4 <albertyso@gmail.com>
2020-06-27 23:57:58 -0400

    add todos route

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const router = express.Router();
 
 router.get('/', (req, res) => res.send('Hello world!'));
 
+router.route('/todos')
+  .get((req, res) => {
+    res.json({}); // send blank json
+  })
+
 app.use('/', router); // hooks things together
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
