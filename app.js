7c167b81551e0b2fd49903f53f36637311671661
aso4 <albertyso@gmail.com>
2020-06-27 23:56:32 -0400

    add base router handler for api calls

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = express.Router();
 
-app.get('/', (req, res) => res.send('Hello world!'));
+router.get('/', (req, res) => res.send('Hello world!'));
+
+app.use('/', router); // hooks things together
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
