fd07febc11a52aa596afcc951da99478dc5cc009
aso4 <albertyso@gmail.com>
2020-06-30 22:13:19 -0400

    add next() call in access control section

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, OPTIONS");
+  next(); // call next middleware
 })
 
 app.use(express.static('public'));
